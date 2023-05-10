0427dec9e89d883b8fdd477f1b329e3ccd5d4387
GeronXXX <lityagin.ru@gmail.com>
2023-05-10 19:48:05 +0300

    v.1.2

--- src/test/java/stream18/ReqreTest.java
+++ src/test/java/stream18/ReqreTest.java
@@ -7,7 +7,7 @@ import static io.restassured.http.ContentType.JSON;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Reqre_test {
+public class ReqreTest {
     @Test
     void checkSingleUserId() {
         Integer expectedId = 2;
